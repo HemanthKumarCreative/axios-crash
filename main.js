75762bfddd620e5ace61f75bdd9460d46a2e295f
Hemanth Kumar <avisihks@gmail.com>
2023-02-21 00:08:30 +0530

    Add functionality to Add, Delete, Get products

--- main.js
+++ main.js
@@ -1,143 +1,53 @@
 // GET REQUEST
 function getTodos() {
-  axios("https://jsonplaceholder.typicode.com/todos?_limit=5")
+  axios("https://crudcrud.com/api/ee5fb260cd484729bf1dd06c6c6fd05d/ecomData")
     .then((res) => showOutput(res))
     .catch((err) => console.error(err));
 }
 
 // POST REQUEST
-function addTodo() {
+function addTodo(event) {
+  event.preventDefault();
+  const bookingObject = {
+    name: document.getElementById("name").value,
+    price: document.getElementById("price").value,
+  };
+
   axios({
     method: "post",
-    url: "https://jsonplaceholder.typicode.com/todos",
-    data: {
-      title: "New Todo",
-      completed: false,
-    },
+    url: "https://crudcrud.com/api/ee5fb260cd484729bf1dd06c6c6fd05d/ecomData",
+    data: bookingObject,
   })
-    .then((res) => showOutput(res))
+    .then(() => getTodos())
     .catch((err) => console.error(err));
+  document.getElementById("myForm").reset();
 }
 
-// PUT/PATCH REQUEST
-function updateTodo() {
-  axios({
-    method: "patch",
-    url: "https://jsonplaceholder.typicode.com/todos/1",
-    data: {
-      title: "Updated Todo",
-      completed: true,
-    },
-  })
-    .then((res) => showOutput(res))
-    .catch((err) => console.error(err));
-}
-
-// DELETE REQUEST
-function removeTodo() {
+//remove
+function removeTodo(event) {
+  let id = event.target.parentElement.parentElement.id;
   axios({
     method: "delete",
-    url: "https://jsonplaceholder.typicode.com/todos/1",
+    url: `https://crudcrud.com/api/ee5fb260cd484729bf1dd06c6c6fd05d/ecomData/${id}`,
   })
-    .then((res) => showOutput(res))
+    .then(() => getTodos())
     .catch((err) => console.error(err));
 }
-
-// SIMULTANEOUS DATA
-function getData() {
-  axios
-    .all([
-      axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5"),
-      axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5"),
-    ])
-    .then((res) => {
-      console.log(res[0]);
-      console.log(res[1]);
-      showOutput(res[1]);
-    })
-    .catch((err) => console.error(err));
-}
-
-// CUSTOM HEADERS
-function customHeaders() {}
-
-// TRANSFORMING REQUESTS & RESPONSES
-function transformResponse() {
-  console.log("Transform Response");
-}
-
-// ERROR HANDLING
-function errorHandling() {
-  console.log("Error Handling");
-}
-
-// CANCEL TOKEN
-function cancelToken() {
-  console.log("Cancel Token");
-}
-
-// INTERCEPTING REQUESTS & RESPONSES
-axios.interceptors.request.use(
-  (config) => {
-    console.log(
-      `${config.method.toUpperCase()} request sent to ${
-        config.url
-      } at ${new Date().getTime()}`
-    );
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-// AXIOS INSTANCES
 
 // Show output in browser
 function showOutput(res) {
-  document.getElementById("res").innerHTML = `
-  <div class="card card-body mb-4">
-    <h5>Status: ${res.status}</h5>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Headers
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-    </div>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Data
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.data, null, 2)}</pre>
-    </div>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Config
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.config, null, 2)}</pre>
-    </div>
-  </div>
-`;
+  document.getElementById("res").innerHTML = ``;
+  res.data
+    .reverse()
+    .slice(0, 5)
+    .forEach((element) => {
+      document.getElementById("res").innerHTML += `
+    <li id="${element._id}"><pre>${element.name} - ${element.price} <button class="btn btn-danger" id="delete" onclick="removeTodo(event)">DELETE</button> <button class="btn btn-warning" id="update" onclick="removeTodo(event)">UPDATE</button></pre></li>
+ `;
+    });
 }
 
 // Event listeners
-document.getElementById("get").addEventListener("click", getTodos);
 document.getElementById("post").addEventListener("click", addTodo);
-document.getElementById("update").addEventListener("click", updateTodo);
-document.getElementById("delete").addEventListener("click", removeTodo);
-document.getElementById("sim").addEventListener("click", getData);
-document.getElementById("headers").addEventListener("click", customHeaders);
-document
-  .getElementById("transform")
-  .addEventListener("click", transformResponse);
-document.getElementById("error").addEventListener("click", errorHandling);
-document.getElementById("cancel").addEventListener("click", cancelToken);
+
+window.addEventListener("DOMContentLoaded", getTodos);
